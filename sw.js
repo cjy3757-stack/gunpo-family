b1afa567ceff0d99066d5465b7f51b964cc8b142
cjy3757-stack <cjy3757@gmail.com>
2026-07-14 22:22:05 +0900

    v6.2
    공지사항, 행사 공지 보완

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='gunpo-eunhye-family-v6.1.0';
-const ASSETS=['./','./index.html','./data.js','./service-data.js','./manifest.webmanifest','./logo.png','./icon-192.png','./icon-512.png'];
+const CACHE='gunpo-eunhye-family-v6.2.0';
+const ASSETS=['./','./index.html','./data.js','./service-data.js','./manifest.webmanifest','./logo.png','./icon-192.png','./icon-512.png','./공지사항.xlsx','./교회일정.xlsx'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r;}).catch(()=>caches.match(e.request)));});
