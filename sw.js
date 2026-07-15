b18135c85d605bff3ec73497aeafdf9312fd1dfe
cjy3757-stack <cjy3757@gmail.com>
2026-07-15 23:28:43 +0900

    재정정보 추가메뉴_v8.0
    재정정보 추가메뉴_v8.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='gunpo-eunhye-family-v7.5.0';
-const ASSETS=['./','./index.html','./data.js','./service-data.js','./office-data.js','./manifest.webmanifest','./logo.png','./icon-192.png','./icon-512.png'];
+const CACHE='gunpo-eunhye-family-v8.0.0';
+const ASSETS=['./','./index.html','./data.js','./service-data.js','./office-data.js','./finance-data.js','./manifest.webmanifest','./logo.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r;}).catch(()=>caches.match(e.request)));});
