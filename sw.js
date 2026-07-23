3d7e705a1c8f21a1e8f35739b9ed02ef4780c6e1
cjy3757-stack <cjy3757@gmail.com>
2026-07-23 09:48:19 +0900

    온라인 출석표시 수정
    온라인 출석표시 수정

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gunpo-eunhye-family-v9.1-20260722';
+const CACHE='gunpo-eunhye-family-v9.1.1-20260723';
 const STATIC=['./','./index.html','./data.js','./service-data.js','./office-data.js','./office-excel.js','./jszip.min.js','./finance-data.js','./manifest.webmanifest','./logo.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
